6057d506e84ed8374999a98f3cbf037f356865f5
Istiak Morsalin <istiak.morsalin@monstar-lab.com>
2023-04-15 16:19:39 +0600

    freshen up repo

--- env/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/Users/istiakmorsalin/iOffice/embedding-search/env/lib"
-XML2_LIBS="-lxml2 -L/Users/istiakmorsalin/iOffice/embedding-search/env/lib -lz -L/Users/istiakmorsalin/iOffice/embedding-search/env/lib -llzma -lpthread -L/Users/istiakmorsalin/iOffice/embedding-search/env/lib -liconv -L/Users/istiakmorsalin/iOffice/embedding-search/env/lib -licui18n -licuuc -licudata -lm "
-XML2_INCLUDEDIR="-I/Users/istiakmorsalin/iOffice/embedding-search/env/include/libxml2 -I/Users/istiakmorsalin/iOffice/embedding-search/env/include"
-MODULE_VERSION="xml2-2.9.14"
-
--- env/lib/xsltConf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the xslt library
-#
-XSLT_LIBDIR="-L/Users/istiakmorsalin/iOffice/embedding-search/env/lib"
-XSLT_LIBS="-lxslt -L/Users/istiakmorsalin/iOffice/embedding-search/env/lib -lxml2 -L/Users/istiakmorsalin/iOffice/embedding-search/env/lib -L/Users/istiakmorsalin/iOffice/embedding-search/env/lib -lpthread -L/Users/istiakmorsalin/iOffice/embedding-search/env/lib -liconv -L/Users/istiakmorsalin/iOffice/embedding-search/env/lib -licui18n -licuuc -licudata -lm "
-XSLT_PRIVATE_LIBS=""
-XSLT_INCLUDEDIR="-I/Users/istiakmorsalin/iOffice/embedding-search/env/include"
-MODULE_VERSION="xslt-1.1.35"
