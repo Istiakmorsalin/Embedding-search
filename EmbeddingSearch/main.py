620279152936317a2a49cb3ab18333bdd54b9e75
Istiak Morsalin <anik.cse@gmail.com>
2023-05-03 13:35:18 +0600

    main refactoring

--- EmbeddingSearch/main.py
+++ EmbeddingSearch/main.py
@@ -1,14 +1,8 @@
-import random
-import numpy as np
-import pandas as pd
-import time
 from dataloader import DataLoader
-from embeddinggenerator import EmbeddingGenerator
 
 def main():
     dataloader = DataLoader()
-    dataloader.loadData()
-    embeddinggenerator = EmbeddingGenerator()
-
+    dataloader.build_index()
+    
 if __name__ == '__main__':
     main()
