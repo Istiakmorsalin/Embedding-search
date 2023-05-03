d803bd26641d68bddd99969f3379d11120fcf467
Istiak Morsalin <anik.cse@gmail.com>
2023-05-03 13:34:00 +0600

    data loader

--- EmbeddingSearch/dataloader.py
+++ EmbeddingSearch/dataloader.py
@@ -25,11 +25,11 @@ class DataLoader:
         # Load Product data and truncate long text fields
         all_prods_df = pd.read_csv('data/product_image_data.csv')
         all_prods_df['primary_key'] = all_prods_df['item_id'] + '-' + all_prods_df['domain_name']
-        print(all_prods_df.shape)
-        print(all_prods_df.head(5))
-        
+        #print(all_prods_df.shape)
+        #print(all_prods_df.head(5))
         subset_df = all_prods_df.head(self.NUMBER_PRODUCTS)
-        print(subset_df.iloc[0])
+        #print(subset_df.iloc[0])
+        return all_prods_df
 
     def build_index(self):
         PRODUCT_IMAGE_VECTOR_FIELD='product_image_vector'
@@ -38,19 +38,21 @@ class DataLoader:
         redis_conn = RedisConnector().connect()
 
         #flush all data
-        redis_conn.flushall()
+        #redis_conn.flushall()
 
         #load data
         product_metadata = self.loadData()
 
         #generate image vectors
         image_path = 'data/images/'
-        img2vec = Img2Vec(cuda=False)
+       # img2vec = Img2Vec(cuda=False)
         img2vec_dict = EmbeddingGenerator().generate_img2vec_dict(product_metadata,image_path)
+        print("Anik")
+        print(img2vec_dict)
 
         #create flat index & load vectors
-        self.create_flat_index(redis_conn, PRODUCT_IMAGE_VECTOR_FIELD, self.NUMBER_PRODUCTS, IMAGE_VECTOR_DIMENSION,'COSINE')
-        self.load_vectors(redis_conn, product_metadata, img2vec_dict, PRODUCT_IMAGE_VECTOR_FIELD)
+        #self.create_flat_index(redis_conn, PRODUCT_IMAGE_VECTOR_FIELD, self.NUMBER_PRODUCTS, IMAGE_VECTOR_DIMENSION,'COSINE')
+       # self.load_vectors(redis_conn, product_metadata, img2vec_dict, PRODUCT_IMAGE_VECTOR_FIELD)
 
         
     def create_flat_index (redis_conn,vector_field_name,number_of_vectors, vector_dimensions=512, distance_metric='L2'):
